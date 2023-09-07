491febafd040091064ad8ebab3e9aadc4b4603ae
nurgeldi <nurgit01@mail.ru>
2023-09-07 15:12:24 +0600

    fix bug

--- lib/features/home/widget/my_apps_widget.dart
+++ lib/features/home/widget/my_apps_widget.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:neonyx/features/common/neo_colors.dart';
 import 'package:neonyx/features/home/data/models/banner_local_model.dart';
